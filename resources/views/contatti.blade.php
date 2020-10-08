41b6256bf1350bf4184b37fa3263d1f3321065d0
Guido <guido.amodio@gmail.com>
2020-10-08 17:17:49 +0200

    laravel

---
+++ resources/views/contatti.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Contatti</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+
+
+    @include('partials.header')
+    @include('contatti.main')
+    @include('partials.footer')
---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>La Molisana</title>
+        <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    </head>
+
+
+    @include('partials.header')
+    @include('home.main')
+    @include('partials.footer')
